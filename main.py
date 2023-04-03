d7c665af4d59d5a94bb99c596ee79b23df3c9581
MuhammadHreden <103263733+MuhammadHreden@users.noreply.github.com>
2023-04-03 22:54:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,25 @@ import time
 def execute():
     url = 'https://visitjordan.gov.jo/travelcars/'
     head = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'}
-    file = pd.read_csv(r'final.csv', header=0)
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'
+        'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+        'accept-encoding' : 'gzip, deflate, br'
+        'accept-language' : 'en-GB,en;q=0.9,en-US;q=0.8'
+        'cache-control': 'max-age=0'
+        'content-length' : '2755'
+        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryiHJNWyPfqTXtBhb1'
+        'origin': 'null'
+        'sec-ch-ua': '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"'
+        'sec-ch-ua-mobile': '?0'
+        'sec-ch-ua-platform': '"Windows"'
+        'sec-fetch-dest': 'document'
+        'sec-fetch-mode': 'navigate'
+        'sec-fetch-site': 'cross-site'
+        'sec-fetch-user': '?1'
+        'upgrade-insecure-requests': '1'
+    
+    }
+    file = pd.read_csv('final.csv', header=0)
     for i in range(len(file.index)):
         data = {
             'gstdoc': file['الوجهة'][i],
@@ -29,12 +46,13 @@ def execute():
             image_data = f.read()
         files = {'hdFileUpload2': ('image.jpg', image_data)}
         r = rq.post(url, headers=head, data=data, files=files)
+        print(file['الاسم'][i])
         print(r)
 
 def start():
     while True:
         now = datetime.datetime.now()
-        if now.hour == 21 and 0 <= now.minute < 30:
+        if now.hour == 21 and 0 <= now.minute < 5:
             execute()
         time.sleep(1)
 
