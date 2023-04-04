0ab5f0084f0c318dd98377bfab3ed0ab4e529fff
MuhammadHreden <103263733+MuhammadHreden@users.noreply.github.com>
2023-04-04 04:50:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ import time
 def execute():
     url = 'https://visitjordan.gov.jo/travelcars/'
     head = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'
-        'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
-        'accept-encoding' : 'gzip, deflate, br'
-        'accept-language' : 'en-GB,en;q=0.9,en-US;q=0.8'
-        'cache-control': 'max-age=0'
-        'content-length' : '2755'
-        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryiHJNWyPfqTXtBhb1'
-        'origin': 'null'
-        'sec-ch-ua': '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"'
-        'sec-ch-ua-mobile': '?0'
-        'sec-ch-ua-platform': '"Windows"'
-        'sec-fetch-dest': 'document'
-        'sec-fetch-mode': 'navigate'
-        'sec-fetch-site': 'cross-site'
-        'sec-fetch-user': '?1'
-        'upgrade-insecure-requests': '1'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54',
+        'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding' : 'gzip, deflate, br',
+        'accept-language' : 'en-GB,en;q=0.9,en-US;q=0.8',
+        'cache-control': 'max-age=0',
+        'content-length' : '2755',
+        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryiHJNWyPfqTXtBhb1',
+        'origin': 'null',
+        'sec-ch-ua': '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
     
     }
     file = pd.read_csv('final.csv', header=0)
