859cc9631c16942cedbf592d4e2ee691e2bc437c
MuhammadHreden <103263733+MuhammadHreden@users.noreply.github.com>
2023-04-06 15:41:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,26 +40,23 @@ def execute():
             'txtIDNumber': file['الرقم الوطني'][i],
             'txtCarNumber': file['رقم السيارة'][i],
             'txtEmail': file['البريد الالكتروني'][i],
-            'ddlCountryCode': file['رمز الدولة'][i],
-            'txtMobile': file['رقم الاتصال'][i],
+            'ddlCountryCode': "00"+str(file['رمز الدولة'][i]),
+            'txtMobile': "0"+str(file['رقم الاتصال'][i]),
             'chAgreed3': 'true',
             'SubmitInvest': 'إرسال الطلب',
         }
 
         image_path = file['جواز السفر'][i] + '.jpg'
-        with open(image_path, 'rb') as f:
-            image_data = f.read()
-        files = {'hdFileUpload2': ('image.jpg', image_data)}
+        files = {'hdFileUpload2': open(image_path, 'rb')}
         r = rq.post(url, headers=head, data=data, files=files)
-        print(file['الاسم'][i])
         print(r)
 
 def start():
     while True:
         now = datetime.datetime.now()
-        if now.hour == 21 and 0 <= now.minute < 5:
+        if now.hour == 12 and 43 <= now.minute < 44:
             execute()
-        time.sleep(1)
+        
 
 
 
